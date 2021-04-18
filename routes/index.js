e45e699457f24a02406ab9a5f1a5c6908c4e2c8a
scmstr <scmstr@hotmail.com>
2021-04-17 18:12:11 -0700

    privacy change

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const ToDos = require("../ToDos");
 // edited to include my non-admin, user level account and PW on mongo atlas
 // and also to include the name of the mongo DB that the collection
 const dbURI =
-"mongodb+srv://Admin:headcrab@jessieclustertest.ecxbf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+"YOUR MONOGO CONNECTION STRING HERE";
 
 // Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
 // by default, you need to set it to false.
